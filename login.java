a5b3a6f11662d1df349bdb8327002ab8bcab1d8a
AkshayKShuklaa <2k22.csaiml.32598@gmail.com>
2024-11-17 18:11:59 +0530

    commit 2

--- login.java
+++ login.java
@@ -5,7 +5,7 @@ class login extends JFrame
 {
     login()
     {
-        setSize(800,400);
+        setSize(800,300);
         setTitle("Automatic Teller Machine");
         setLayout(null);
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("logo.jpg"));
