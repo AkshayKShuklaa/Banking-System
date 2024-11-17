a48b5200db1af1856592e4179e9c26f77b80a572
Akshay Kumar Shukla <shuklaakshay1234@gmail.com>
2024-11-17 16:11:17 +0530

    First Commit

---
+++ login.java
@@ -0,0 +1,29 @@
+
+import java.awt.Image;
+import javax.swing.*;
+class login extends JFrame
+{
+    login()
+    {
+        setSize(800,400);
+        setTitle("Automatic Teller Machine");
+        setLayout(null);
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("logo.jpg"));
+        Image i2=i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel label=new JLabel(i3);
+
+        setBounds(70,   10, 100, 100);
+
+        add(label);
+        setVisible(true);
+        setLocation(350, 200);
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("HELLO JAVA FSD");
+        login ob=new login();
+    }    
+}
